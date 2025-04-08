99bc54bdcdda402a5c96df242ea82bc3de58c4da
Khaerul743 <khaerullutfi028@gmail.com>
2025-04-08 11:29:49 +0700

    Connect database

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const { User } = require("./models/user");
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+  User.create({
+    name: "Khaerul",
+    email: "Khaerul@gmail.com",
+    password: "12345678",
+  });
 });
 
 app.listen("3000", () => {
---
+++ Backend/models/user.js
@@ -0,0 +1,41 @@
+const sequelize = require("../config/db_connet");
+const { DataTypes } = require("sequelize");
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM("admin", "user"),
+      defaultValue: "user",
+    },
+  },
+  {
+    tableName: "users",
+    timestamps: false,
+  }
+);
+
+module.exports = { User };
